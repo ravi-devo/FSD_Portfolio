97c008ceadd18d5b1ed5aedcf25e7dba88af835e
ravi-devo <ravi.ravichandran370@gmail.com>
2024-06-05 21:16:15 +0530

    Button update on project

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -1,4 +1,4 @@
-import { Card } from "react-bootstrap";
+import { Card, Button } from "react-bootstrap";
 import faceOfTamizhanImage from '../assets/projectSnips/faceOfTamizhan.png';
 import serviceAnytime from '../assets/projectSnips/serviceAnytime.png';
 import expenseTrackerImage from '../assets/projectSnips/expenseTracker.png';
@@ -6,33 +6,68 @@ import taskManager from '../assets/projectSnips/taskManager.png';
 import '../styles/projects.css';
 
 const MyProjects = () => {
-    return (<>
+    return (
         <div className="projects-component my-4">
             <h1 className='text-center mb-5'>My Projects</h1>
             <div className="projects-div my-4">
                 <Card className="image-Card">
-                    <a href="https://github.com/ravi-devo/faceOfTamizhan-frontend" target="_blank" rel="noopener noreferrer">
-                        <Card.Img src={faceOfTamizhanImage} alt="Face-Of-Tamizan project" variant="top" />
-                    </a>
+                    <Card.Img src={faceOfTamizhanImage} alt="Face-Of-Tamizan project" variant="top" />
+                    <Card.Body className="text-center">
+                        <Button onClick={() => window.open('https://main.d1uzfwgx5uwlqf.amplifyapp.com/', '_blank', 'noopener noreferrer')}
+                            variant="primary" className="m-1">Demo</Button>
+                        <Button
+                            onClick={() => window.open('https://github.com/ravi-devo/faceOfTamizhan-frontend', '_blank', 'noopener noreferrer')}
+                            variant="secondary" className="m-1">Frontend Code</Button>
+                        <Button
+                            onClick={() => window.open('https://github.com/ravi-devo/faceOfTamizhan-backend', '_blank', 'noopener noreferrer')}
+                            variant="success" className="m-1">Backend Code</Button>
+                    </Card.Body>
                 </Card>
                 <Card className="image-Card">
-                    <a href="https://github.com/ravi-devo/serviceAnytime_FE" target="_blank" rel="noopener noreferrer">
-                        <Card.Img src={serviceAnytime} alt="Service anytime project" variant="top" />
-                    </a>
+                    <Card.Img src={serviceAnytime} alt="Service anytime project" variant="top" />
+                    <Card.Body className="text-center">
+                        <Button
+                            onClick={() => window.open('https://main.d3m137m9inkje1.amplifyapp.com/', '_blank', 'noopener noreferrer')}
+                            variant="primary" className="m-1">Demo</Button>
+                        <Button
+                            onClick={() => window.open('https://github.com/ravi-devo/serviceAnytime_FE', '_blank', 'noopener noreferrer')}
+                            variant="secondary" className="m-1">Frontend Code</Button>
+                        <Button
+                            onClick={() => window.open('https://github.com/ravi-devo/serviceAnytime_BE', '_blank', 'noopener noreferrer')}
+                            variant="success" className="m-1">Backend Code</Button>
+                    </Card.Body>
                 </Card>
                 <Card className="image-Card">
-                    <a href="https://github.com/ravi-devo/expenseTracker_FE" target="_blank" rel="noopener noreferrer">
-                        <Card.Img src={expenseTrackerImage} alt="Expense tracker project" variant="top" />
-                    </a>
+                    <Card.Img className="mb-3" src={expenseTrackerImage} alt="Expense tracker project" variant="top" />
+                    <Card.Body className="text-center">
+                        <Button
+                            onClick={() => window.open('https://main.d4ed8we1flo2r.amplifyapp.com/', '_blank', 'noopener noreferrer')}
+                            variant="primary" className="m-1">Demo</Button>
+                        <Button
+                            onClick={() => window.open('https://github.com/ravi-devo/expenseTracker_FE', '_blank', 'noopener noreferrer')}
+                            variant="secondary" className="m-1">Frontend Code</Button>
+                        <Button
+                            onClick={() => window.open('https://github.com/ravi-devo/expenseTracker_BE', '_blank', 'noopener noreferrer')}
+                            variant="success" className="m-1">Backend Code</Button>
+                    </Card.Body>
                 </Card>
                 <Card className="image-Card">
-                    <a href="https://github.com/ravi-devo/taskManager_FE" target="_blank" rel="noopener noreferrer">
-                        <Card.Img src={taskManager} alt="Task manager project" variant="top" />
-                    </a>
+                    <Card.Img className="mb-3" src={taskManager} alt="Expense tracker project" variant="top" />
+                    <Card.Body className="text-center">
+                        <Button
+                            onClick={() => window.open('https://main.dvin4agi4z8cq.amplifyapp.com/', '_blank', 'noopener noreferrer')}
+                            variant="primary" className="m-1">Demo</Button>
+                        <Button
+                            onClick={() => window.open('https://github.com/ravi-devo/taskManager_FE', '_blank', 'noopener noreferrer')}
+                            variant="secondary" className="m-1">Frontend Code</Button>
+                        <Button
+                            onClick={() => window.open('https://github.com/ravi-devo/taskManager_BE', '_blank', 'noopener noreferrer')}
+                            variant="success" className="m-1">Backend Code</Button>
+                    </Card.Body>
                 </Card>
             </div>
         </div>
-    </>)
+    );
 }
 
 export default MyProjects;
